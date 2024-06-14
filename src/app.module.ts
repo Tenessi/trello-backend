d93fcecbec5313b09e399e5efa96935b1f7929f3
Tenessi <krotov.sergey.017@mail.ru>
2024-06-14 15:06:20 +0600

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { TaskModule } from './task/task.module';
+
+@Module({
+  imports: [AuthModule, ConfigModule.forRoot(), UserModule, TaskModule]
+})
+export class AppModule {}
